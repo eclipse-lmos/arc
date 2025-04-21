f0df5fd94e68963e8414e3959ab0944bd0de1581
patrick.whelan@telekom.de <patrick.whelan@telekom.de>
2025-04-21 11:48:20 +0200

    examples: Add openai example

---
+++ examples/src/main/kotlin/OpenAIAgent.kt
@@ -0,0 +1,39 @@
+// SPDX-FileCopyrightText: 2025 Deutsche Telekom AG and others
+//
+// SPDX-License-Identifier: Apache-2.0
+package org.eclipse.lmos.arc.agents.examples
+
+import kotlinx.coroutines.runBlocking
+import org.eclipse.lmos.arc.agents.agent.ask
+import org.eclipse.lmos.arc.agents.agents
+import org.eclipse.lmos.arc.agents.getChatAgent
+import org.eclipse.lmos.arc.core.getOrNull
+
+fun main() = runBlocking {
+    // Only the api key is required.
+    System.setProperty("OPENAI_API_KEY", "1234")
+
+    val agents = agents(
+        functions = {
+            function(
+                name = "get_weather",
+                description = "Get the weather for a given location.",
+            ) {
+                "THe weather is sunny"
+            }
+        },
+    ) {
+        agent {
+            name = "MyAgent"
+            model { "gpt-4o" }
+            prompt {
+                """
+                You are a weather assistant. Help the user with their questions about the weather.
+                """
+            }
+        }
+    }
+
+    val reply = agents.getChatAgent("MyAgent").ask("What is the weather like today?").getOrNull()
+    println(reply)
+}
