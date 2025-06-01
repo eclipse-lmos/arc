361a3c133bd8a996672b6103c00b5f4e3d0d86ac
patrick.whelan@telekom.de <patrick.whelan@telekom.de>
2025-06-01 18:00:35 +0200

    feat(graphql): add tools mutation.

--- arc-graphql-spring-boot-starter/src/main/kotlin/inbound/ToolMutation.kt
+++ arc-graphql-spring-boot-starter/src/main/kotlin/inbound/ToolMutation.kt
@@ -9,6 +9,8 @@ import com.expediagroup.graphql.server.operations.Mutation
 import kotlinx.serialization.Serializable
 import org.eclipse.lmos.arc.agents.functions.LLMFunctionProvider
 import org.eclipse.lmos.arc.agents.functions.toJsonMap
+import org.eclipse.lmos.arc.core.Failure
+import org.eclipse.lmos.arc.core.Success
 import org.eclipse.lmos.arc.core.getOrThrow
 
 /**
@@ -17,14 +19,16 @@ import org.eclipse.lmos.arc.core.getOrThrow
 class ToolMutation(private val functionProvider: LLMFunctionProvider) : Mutation {
 
     @GraphQLDescription("Executes a tool with the given name and parameters.")
-    suspend fun tool(toolExecution: ToolExecution): String {
-        val params = toolExecution.parameters.toJsonMap()
-        return functionProvider.provide(toolExecution.name).getOrThrow().execute(params).getOrThrow()
+    suspend fun tool(name: String, parameters: String): ToolExecutionResult {
+        val params = parameters.toJsonMap()
+        val result = functionProvider.provide(name).getOrThrow().execute(params)
+        return when (result) {
+            is Success -> ToolExecutionResult(result = result.value)
+            is Failure -> ToolExecutionResult(error = result.reason.message ?: "Unknown error")
+            else -> ToolExecutionResult(error = "Unknown error")
+        }
     }
 }
 
 @Serializable
-data class ToolExecution(
-    val name: String,
-    val parameters: String,
-)
+data class ToolExecutionResult(val result: String? = null, val error: String? = null)
