02bf2fe23a10c485c4ddf44a0a37ed946e4f324c
patrick.whelan@telekom.de <patrick.whelan@telekom.de>
2025-06-25 21:44:09 +0200

    feat(featureFags): add featureFags

--- arc-graphql-spring-boot-starter/src/main/kotlin/AgentResolver.kt
+++ arc-graphql-spring-boot-starter/src/main/kotlin/AgentResolver.kt
@@ -5,6 +5,8 @@
 package org.eclipse.lmos.arc.graphql
 
 import org.eclipse.lmos.arc.agents.Agent
+import org.eclipse.lmos.arc.agents.AgentProvider
+import org.eclipse.lmos.arc.agents.features.FeatureFlags
 import org.eclipse.lmos.arc.api.AgentRequest
 
 /**
@@ -14,4 +16,23 @@ import org.eclipse.lmos.arc.api.AgentRequest
 interface AgentResolver {
 
     fun resolveAgent(agentName: String? = null, request: AgentRequest): Agent<*, *>?
+}
+
+/**
+ * Resolves the agent based on feature flags.
+ * Returns the first agent that has all its feature flags enabled.
+ */
+class FeatureAgentResolver(
+    private val features: FeatureFlags,
+    private val agentProvider: AgentProvider,
+) : AgentResolver {
+
+    override fun resolveAgent(
+        agentName: String?,
+        request: AgentRequest,
+    ): Agent<*, *>? {
+        return agentProvider.getAgents().firstOrNull {
+            it.onFlags.isNotEmpty() && it.onFlags.all { features.isFeatureEnabled(it) }
+        }
+    }
 }
