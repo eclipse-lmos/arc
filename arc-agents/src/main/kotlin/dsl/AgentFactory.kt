3c8b321f5853882fbd3689d05fd8a97d48f9a477
patrick.whelan@telekom.de <patrick.whelan@telekom.de>
2025-06-26 11:07:45 +0200

    feat(function): add version field to functions

--- arc-agents/src/main/kotlin/dsl/AgentFactory.kt
+++ arc-agents/src/main/kotlin/dsl/AgentFactory.kt
@@ -24,6 +24,7 @@ class ChatAgentFactory(private val beanProvider: BeanProvider) : AgentFactory<Ch
             name = agentDefinition.name,
             description = agentDefinition.description,
             version = agentDefinition.version,
+            onFeatures = agentDefinition.onFeatures,
             skills = agentDefinition.skills,
             model = agentDefinition.model,
             agentDefinition.settings,
