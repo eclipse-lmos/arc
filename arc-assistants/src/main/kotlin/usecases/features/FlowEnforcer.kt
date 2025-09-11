ea0249162cbcff51a846284167e85b624cab2c12
patrick.whelan@telekom.de <patrick.whelan@telekom.de>
2025-09-11 13:23:45 +0200

    feat(usecase): add use case flows

--- arc-assistants/src/main/kotlin/usecases/features/FlowEnforcer.kt
+++ arc-assistants/src/main/kotlin/usecases/features/FlowEnforcer.kt
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2025 Deutsche Telekom AG and others
+//
+// SPDX-License-Identifier: Apache-2.0
 package org.eclipse.lmos.arc.assistants.support.usecases.features
 
 import org.eclipse.lmos.arc.agents.ArcException
