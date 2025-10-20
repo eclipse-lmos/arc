19beefbc56444375966126391d697a3d5f627cd2
patrick.whelan@telekom.de <patrick.whelan@telekom.de>
2025-10-20 12:54:20 +0200

    feat(mcp): add meta data field to tool calls.

--- arc-mcp/src/test/kotlin/McpToolsTest.kt
+++ arc-mcp/src/test/kotlin/McpToolsTest.kt
@@ -3,8 +3,11 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.eclipse.lmos.arc.mcp
 
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
+import org.eclipse.lmos.arc.agents.dsl.withDSLContext
+import org.eclipse.lmos.arc.agents.functions.FunctionWithContext
 import org.eclipse.lmos.arc.core.getOrThrow
 import org.junit.jupiter.api.Test
 
@@ -21,5 +24,17 @@ class McpToolsTest : TestBase() {
         val tools = McpTools("http://localhost:8080", null).load(null)
         val result = tools.first { it.name == "getBooks" }.execute(mapOf("id" to "Kotlin"))
         assertThat(result.getOrThrow()).isEqualTo("""[{"name":"Spring Boot"},{"name":"Kotlin"}]""")
+    }
+
+    @Test
+    fun `test execute tool with metadata`(): Unit = runBlocking {
+        val tools = McpTools("http://localhost:8080", null).load(null)
+        val fn = tools.first { it.name == "getBookDetails" } as FunctionWithContext
+        delay(200_000)
+        withDSLContext(setOf(ToolCallMetadata(data = mapOf("test" to "value")))) {
+            val fnWithContext = fn.withContext(this)
+            val result = fnWithContext.execute(mapOf("id" to "Kotlin"))
+            assertThat(result.getOrThrow()).isEqualTo("ToolCallMetadata(data={test=value})")
+        }
     }
 }
