3809829cdfa4077a06581686a8e5ffc0b363c2fb
patrick.whelan@telekom.de <patrick.whelan@telekom.de>
2025-10-29 17:42:25 +0100

    fix: remove context receivers from gen package.

---
+++ arc-assistants/src/test/kotlin/usecases/FormatToStringTest.kt
@@ -0,0 +1,76 @@
+// SPDX-FileCopyrightText: 2025 Deutsche Telekom AG and others
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package org.eclipse.lmos.arc.assistants.support.usecases
+
+import kotlinx.coroutines.runBlocking
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class FormatToStringTest {
+
+    @Test
+    fun `test format simple use case`(): Unit = runBlocking {
+        val input = """
+            ### UseCase: usecase1
+            #### Description
+            The description of the use case 1.
+            #### Solution 
+            Solution
+            ----
+        """.trimIndent().toUseCases()
+        val result = input.formatToString().trim()
+        assertThat(result).isEqualTo(
+            """
+            ### UseCase: usecase1
+            #### Description
+            The description of the use case 1.
+
+            #### Solution
+            Solution
+
+            ----
+            """.trimIndent(),
+        )
+    }
+
+    @Test
+    fun `test format use case with regex conditional - no match`(): Unit = runBlocking {
+        val input = """
+            ### UseCase: usecase1 <regex:.*uc1.*>
+            #### Description
+            The description of the use case 1.
+            #### Solution 
+            Solution
+            ----
+        """.trimIndent().toUseCases()
+        val result = input.formatToString()
+        assertThat(result).isEqualTo("")
+    }
+
+    @Test
+    fun `test format use case with regex conditional - match`(): Unit = runBlocking {
+        val input = """
+            ### UseCase: usecase1 <regex:.*uc1.*>
+            #### Description
+            The description of the use case 1.
+            #### Solution 
+            Solution
+            ----
+        """.trimIndent().toUseCases()
+        val result = input.formatToString(input = "This is uc1 test").trim()
+        assertThat(result).isEqualTo(
+            """
+            ### UseCase: usecase1
+            #### Description
+            The description of the use case 1.
+
+            #### Solution
+            Solution
+
+            ----
+            """.trimIndent(),
+        )
+    }
+}
