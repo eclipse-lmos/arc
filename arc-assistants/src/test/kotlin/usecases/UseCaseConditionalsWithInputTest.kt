90e43576392f67f0b130fe38c9afc9c897de3347
patrick.whelan@telekom.de <patrick.whelan@telekom.de>
2025-11-04 17:51:01 +0100

    chore: add missing tests

---
+++ arc-assistants/src/test/kotlin/usecases/UseCaseConditionalsWithInputTest.kt
@@ -0,0 +1,64 @@
+// SPDX-FileCopyrightText: 2025 Deutsche Telekom AG and others
+//
+// SPDX-License-Identifier: Apache-2.0
+package org.eclipse.lmos.arc.assistants.support.usecases
+
+import kotlinx.coroutines.runBlocking
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+class UseCaseConditionalsWithInputTest {
+
+    @Test
+    fun `test simple use case conditional - no match`(): Unit = runBlocking {
+        val input = """
+            ### UseCase: usecase1 <foo>
+            #### Description
+            The description of the use case 1.
+            #### Solution 
+            Solution
+            ----
+        """.trimIndent().toUseCases()
+        val result = input.formatToString(input = "hi").trim()
+        Assertions.assertThat(result).isEqualTo("")
+    }
+
+    @Test
+    fun `test simple use case conditional - no match multiple conditions`(): Unit = runBlocking {
+        val input = """
+            ### UseCase: usecase1 <foo, bar>
+            #### Description
+            The description of the use case 1.
+            #### Solution 
+            Solution
+            ----
+        """.trimIndent().toUseCases()
+        val result = input.formatToString(input = "hi", conditions = setOf("foo")).trim()
+        Assertions.assertThat(result).isEqualTo("")
+    }
+
+    @Test
+    fun `test simple use case conditional - match`(): Unit = runBlocking {
+        val input = """
+            ### UseCase: usecase1 <foo>
+            #### Description
+            The description of the use case 1.
+            #### Solution 
+            Solution
+            ----
+        """.trimIndent().toUseCases()
+        val result = input.formatToString(input = "hi", conditions = setOf("foo")).trim()
+        Assertions.assertThat(result).isEqualTo(
+            """
+           |### UseCase: usecase1
+           |#### Description
+           |The description of the use case 1.
+           |
+           |#### Solution
+           |Solution
+           |
+           |----
+            """.trimMargin(),
+        )
+    }
+}
