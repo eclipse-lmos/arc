2a013e290222bbc263e04e06fab80c141b6fb438
patrick.whelan@telekom.de <patrick.whelan@telekom.de>
2026-01-06 19:33:04 +0100

    feat: add example agent to adl service

--- adl-server/src/main/kotlin/agents/ExampleAgent.kt
+++ adl-server/src/main/kotlin/agents/ExampleAgent.kt
@@ -12,14 +12,7 @@ fun createExampleAgent(): ConversationAgent = agents {
         prompt {
             """
               Generate 20 examples that match the following use case description.
-              Do not mention company names or products in the examples unless they are explicitly part of the use case description.
-              
-              ## OUTPUT FORMAT
-              Output the examples as a markdown list. Example:
-                - Example 1
-                - Example 2
-                - Example 3
-            """
+                """
         }
     }
 }.getAgents().first() as ConversationAgent
