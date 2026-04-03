3e53c52dc4d4944515f535b1dc8a7a2b02d08f07
patrick.whelan@telekom.de <patrick.whelan@telekom.de>
2026-04-03 13:07:52 +0200

    feat: add UseCaseMatcher for improved use case classification and integrate with UseCaseResponseHandler

--- arc-assistants/src/main/kotlin/filters/UseCaseMatcher.kt
+++ arc-assistants/src/main/kotlin/filters/UseCaseMatcher.kt
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Deutsche Telekom AG and others
+//
+// SPDX-License-Identifier: Apache-2.0
+
 package org.eclipse.lmos.arc.assistants.support.filters
 
 import org.eclipse.lmos.arc.agents.conversation.ConversationMessage
